a955afc17dd65a33089d2cdfa5424d8accaec600
Pedro Santos <pedrosantos200206@gmail.com>
2022-10-25 22:28:37 -0300

    site copia barata da netflix

---
+++ app.js
@@ -0,0 +1,14 @@
+let filme1 = 'imagens/filme01.jpg'
+let filme2 = 'imagens/filme02.jpg'
+let filme3 = 'imagens/filme03.jpg'
+let filme4 = 'imagens/filme04.jpg'
+let filme5 = 'imagens/filme05.jpg'
+let filme6 = 'imagens/filme06.jpg'
+
+
+document.querySelector(".container").innerHTML += "<img src='" + filme1 + "'>"
+document.querySelector(".container").innerHTML += "<img src='" + filme2 + "'>"
+document.querySelector(".container").innerHTML += "<img src='" + filme3 + "'>"
+document.querySelector(".container").innerHTML += "<img src='" + filme4 + "'>"
+document.querySelector(".container").innerHTML += "<img src='" + filme5 + "'>"
+document.querySelector(".container").innerHTML += "<img src='" + filme6 + "'>"
